b73a1bd9a5f6dc83779e2d205e40dfabb2a34ca5
syahn <496300@naver.com>
2023-04-30 14:35:30 +0900

    백준 n25304 : 영수증

---
+++ 그린_안승연/week5/n25304.java
@@ -0,0 +1,23 @@
+package 그린_안승연.week5;
+
+import java.util.Scanner;
+
+public class n25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long total = sc.nextLong();
+        long sum = 0L;
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int price = sc.nextInt();
+            int num = sc.nextInt();
+            sum += price * num;
+        }
+
+        if (sum == total)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
