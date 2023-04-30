4a14d300cee51f220b0eb2c69bb065bfbf251a5c
syahn <496300@naver.com>
2023-04-30 15:08:16 +0900

    백준 n10807 : 개수 세기

---
+++ 그린_안승연/week5/n10807.java
@@ -0,0 +1,21 @@
+package 그린_안승연.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] nums = br.readLine().split(" ");
+        String v = br.readLine();
+
+        int count = 0;
+
+        for (String s : nums)
+            if (s.equals(v)) count++;
+
+        System.out.println(count);
+    }
+}
