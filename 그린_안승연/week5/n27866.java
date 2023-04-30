5f1dbb92c0d5bedc030b07f9e30366b8e217c4e2
syahn <496300@naver.com>
2023-04-30 15:25:24 +0900

    백준 n27866 : 문자와 문자열

---
+++ 그린_안승연/week5/n27866.java
@@ -0,0 +1,14 @@
+package 그린_안승연.week5;
+
+import java.util.Scanner;
+
+public class n27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int i = sc.nextInt();
+
+        System.out.println(String.valueOf(s.charAt(i - 1)));
+
+    }
+}
