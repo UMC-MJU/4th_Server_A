ed87d27c58a6212ce64a32e15313fbd66dab9666
이승학 <iseunghag@iseunghag-ui-MacBookAir.local>
2023-05-07 00:07:15 +0900

    feature:6주차 java,javascript

---
+++ 학이_이승학/week6/week6_java/Student.java
@@ -0,0 +1,19 @@
+package 학이_이승학.week6.week6_java;
+
+public class Student {
+    int studentId;
+    String studentName;
+    String major;
+
+    public Student(int studentIdx, String studentName, String major) {
+        this.studentId = studentIdx;
+        this.studentName = studentName;
+        this.major = major;
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + studentName);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Major: " + major);
+    }
+}
