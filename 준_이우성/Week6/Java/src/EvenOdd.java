918d99e4a3dadd54d0e5e5553cb4d73aa0a3b434
June <dtd1614@gmail.com>
2023-05-08 00:18:39 +0900

    feat : 6주차 미션

---
+++ 준_이우성/Week6/Java/src/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static boolean isEven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
