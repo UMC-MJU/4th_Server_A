93d7bd6331c8aaa96ef7cd1434970d97a09f3c69
June <dtd1614@gmail.com>
2023-05-08 00:15:47 +0900

    delete week6

--- 준_이우성/Week6/Java/src/StudentMain.java
+++
@@ -1,6 +0,0 @@
-public class StudentMain {
-    public static void main(String[] args) {
-        Student student = new Student("이우성", 60181924, "융합소프트웨어학부");
-        student.printInfo();
-    }
-}
