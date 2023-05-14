a27ffd31e5d4599306f4b8e65bcfc59d80220b89
이승학 <iseunghag@iseunghag-ui-MacBookAir.local>
2023-05-14 21:49:23 +0900

    test

--- 준_이우성/Week6/Java/src/EvenOddMain.java
+++
@@ -1,9 +0,0 @@
-public class EvenOddMain {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (EvenOdd.isEven(i)) {
-                System.out.println(i + " is even.");
-            }
-        }
-    }
-}
