8c2bc54826a099a030003ac8ad2a4141aa755fbf
syahn <496300@naver.com>
2023-05-14 23:34:11 +0900

    feat: 게시물 Service 생성

---
+++ 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Service/BoardService.java
@@ -0,0 +1,48 @@
+package com.example.umc_board.Service;
+
+import com.example.umc_board.Dto.BoardRequestDto;
+import com.example.umc_board.Entity.Board;
+import com.example.umc_board.Entity.Category;
+import com.example.umc_board.Repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public Page<Board> findBoardList(Pageable pageable){
+        return boardRepository
+                .findAll(PageRequest.of(
+                        pageable.getPageNumber() <= 0?0: pageable.getPageNumber()-1, pageable.getPageSize()
+                ));
+    }
+
+    public Board findBoardById(Long id){
+        Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("글이 없습니다."));
+        return board;
+    }
+
+    @Transactional
+    public Board save(BoardRequestDto requestDto){
+        return boardRepository.save(requestDto.toEntity());
+    }
+
+    @Transactional
+    public Board update(Long id, BoardRequestDto requestDto){
+        Board find = findBoardById(id);
+        find.update(requestDto.getTitle(), requestDto.getContent(), Category.valueOf(requestDto.getCategory()));
+        return boardRepository.save(find);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        boardRepository.deleteById(id);
+    }
+}
