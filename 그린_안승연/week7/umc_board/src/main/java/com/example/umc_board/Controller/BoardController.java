b80db510fcc807425228bfe678b026f54c207d29
syahn <496300@naver.com>
2023-05-22 00:14:12 +0900

    feat: BoardController에 카카오맵 검색 기능 추가

--- 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Controller/BoardController.java
+++ 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Controller/BoardController.java
@@ -1,18 +1,31 @@
 package com.example.umc_board.Controller;
 
-import com.example.umc_board.Service.BoardService;
 import com.example.umc_board.Dto.BoardRequestDto;
 import com.example.umc_board.Dto.BoardResponseDto;
 import com.example.umc_board.Entity.Board;
-import io.swagger.annotations.*;
+import com.example.umc_board.Service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 @RestController
@@ -22,6 +35,12 @@ import java.nio.charset.StandardCharsets;
 public class BoardController {
 
     private final BoardService boardService;
+
+    @Value("${kakao.map.url}")
+    private String KAKAO_MAP_URL;
+
+    @Value("${kakao.map.key}")
+    private String KAKAO_MAP_KEY;
 
     @GetMapping("/list")
     @ApiOperation(value = "게시물 페이지 API")
@@ -39,9 +58,36 @@ public class BoardController {
 
     @PostMapping("/post")
     @ApiOperation(value = "게시물 작성 API")
-    public ResponseEntity<BoardResponseDto> postBoard(@RequestBody BoardRequestDto requestDto){
+    public ResponseEntity<BoardResponseDto> postBoard(@RequestBody BoardRequestDto requestDto) throws MalformedURLException {
         return ResponseEntity.ok(BoardResponseDto.of(boardService.save(requestDto)));
     }
+
+    @GetMapping("/kakaomap/{keyword}")
+    public ResponseEntity<String> keywordMap(@PathVariable(value = "keyword") String keyword) throws IOException, URISyntaxException {
+        ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(keyword);
+        String search = StandardCharsets.UTF_8.decode(byteBuffer).toString();
+
+        URI uri = UriComponentsBuilder
+                .fromUriString(KAKAO_MAP_URL)
+                .path("/v2/local/search/keyword.json") // 요청 경로를 "/v2/local/search/keyword.json"으로 지정
+                .queryParam("query", search)
+                .queryParam("page", 1)
+                .queryParam("size", 10)
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "KakaoAK " + KAKAO_MAP_KEY);
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
+        return result;
+    }
+
 
     @PutMapping("/{id}")
     @ApiOperation(value = "게시물 수정 API")
