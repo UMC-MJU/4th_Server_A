4957c6a3e5efed082ad7d3896d740786e198c820
syahn <496300@naver.com>
2023-05-22 00:13:15 +0900

    feat: board dto에 kakaomap 장소 리스트 추가

--- 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Dto/BoardRequestDto.java
+++ 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Dto/BoardRequestDto.java
@@ -2,11 +2,17 @@ package com.example.umc_board.Dto;
 
 import com.example.umc_board.Entity.Board;
 import com.example.umc_board.Entity.Category;
+import com.example.umc_board.Entity.KakaoPlace;
 import com.example.umc_board.Entity.User;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -24,20 +30,39 @@ public class BoardRequestDto {
     @ApiModelProperty(example = "tester")
     private User user;
 
+    private List<List<String>> placeList;
+
     @Builder
-    public BoardRequestDto(String title, String content, String category, User user){
+    public BoardRequestDto(String title, String content, String category, User user,
+                           List<List<String>> places){
         this.title = title;
         this.content = content;
         this.category = category;
         this.user = user;
+        this.placeList = places;
     }
 
-    public Board toEntity(){
+    public Board toEntity() throws  MalformedURLException {
+        List<KakaoPlace> places = new ArrayList<>();
+
+        for(List<String> place : placeList){
+            Long id = Long.valueOf(place.get(0));
+            String title = place.get(1);
+            URL url = new URL(place.get(2));
+            KakaoPlace temp = KakaoPlace.builder()
+                    .id(id)
+                    .title(title)
+                    .url(url)
+                    .build();
+            places.add(temp);
+        }
+
         return Board.builder()
                 .title(title)
                 .content(content)
                 .category(Category.valueOf(category))
                 .user(user)
+                .list(places)
                 .build();
     }
 }
--- 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Dto/BoardResponseDto.java
+++ 그린_안승연/week7/umc_board/src/main/java/com/example/umc_board/Dto/BoardResponseDto.java
@@ -1,11 +1,15 @@
 package com.example.umc_board.Dto;
 
 import com.example.umc_board.Entity.Board;
+import com.example.umc_board.Entity.KakaoPlace;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -25,12 +29,26 @@ public class BoardResponseDto {
     @ApiModelProperty(example = "tester")
     private String user;
 
+    private List<List<String>> placeList;
+
     public static BoardResponseDto of(Board board){
+        List<KakaoPlace> places = board.getPlaceList();
+        List<List<String>> list = new ArrayList<>();
+
+        for(KakaoPlace place : places){
+            List<String> tempList = new ArrayList<>();
+            tempList.add(place.getId().toString());
+            tempList.add(place.getTitle());
+            tempList.add(place.getUrl().toString());
+            list.add(tempList);
+        }
+
         return BoardResponseDto.builder()
                 .title(board.getTitle())
                 .content(board.getContent())
                 .category(board.getCategory().getValue())
                 .user(board.getUser().getName())
+                .placeList(list)
                 .build();
     }
 }
